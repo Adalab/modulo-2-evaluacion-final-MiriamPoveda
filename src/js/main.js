590c5be4bbef8ba6422be31631337758f0111d97
MiriamPoveda <mpoveda94@gmail.com>
2022-04-08 12:51:24 +0200

    Task 2.4 add + <li> click event/function

--- src/js/main.js
+++ src/js/main.js
@@ -2,29 +2,48 @@
 
 // CONSTANTS //
 
-let cocktailData = [];
-
 const searchButton = document.querySelector('.js_search');
 const searchInput = document.querySelector('.js_searchInput');
 const listCocktails = document.querySelector('.js_listCocktails');
 
+let cocktailData = [];
+
 // FUNCTIONS //
 
-/* PAINT COCKTAILS */
+/* J */
+function selectCocktails () {
+  const favLi = document.querySelectorAll('.js_favLi');
+  for (const itemLi of favLi) {
+    itemLi.addEventListener('click', handleClickFav);
+  }
+}
+
+/* PAINT COCKTAILS + LI CLICK EVENT */
 function paintCocktails() {
   let html = '';
   for (const drink of cocktailData) {
-    html += `<li>`;
+    html += `<li class="js_favLi" id=${drink.idDrink}>`;
     html += `<h3>${drink.strDrink}</h3>`;
     html += `<img src="${drink.strDrinkThumb}" width="100"/>`;
     html += `</li>`;
   }
   listCocktails.innerHTML = html;
+  selectCocktails();
 }
 
-/* HANDLE CLICK SEARCH + FETCH */
-function handleClickSearch(event) {
-  event.preventDefault();
+/* FILL IMAGE */
+function fillImage() {
+  for (const drink of cocktailData) {
+    if (drink.strDrinkThumb === null) {
+      drink.strDrinkThumb = `https://img.freepik.com/vector-gratis/coctel-dibujo-mano-rodaja-limon-romero_218179-270.jpg?w=100`;
+    } else {
+      drink.strDrinkThumb;
+    }
+  }
+}
+
+/* GET API + FETCH */
+function getApi() {
   const searchValue = searchInput.value;
   /* Fetch */
   fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
@@ -32,7 +51,19 @@ function handleClickSearch(event) {
     .then((data) => {
       cocktailData = data.drinks;
       paintCocktails();
+      fillImage();
     });
+}
+
+/* FAV HANDLER FUNCTION */
+function handleClickFav(event) {
+  event.currentTarget.id;
+}
+
+/* SEARCH HANDLER FUNCTION */
+function handleClickSearch(event) {
+  event.preventDefault();
+  getApi();
 }
 
 // EVENTS //
