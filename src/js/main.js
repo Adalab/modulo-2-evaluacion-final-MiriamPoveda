c36cfd5c520163ce9772d5e169dad52548ba6db7
MiriamPoveda <mpoveda94@gmail.com>
2022-04-08 10:11:37 +0200

    Task 2.1 added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// PRINCIPAL VARIABLE //
+
+let cocktailData = [];
+
+// CONSTANTS //
+
+const searchButton = document.querySelector('.js_search');
+
+// FUNCTIONS //
+
+function handleClickSearch(event) {
+  event.preventDefault();
+  /* Fetch */
+  fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then((response) => response.json())
+    .then((data) => {
+      cocktailData = data.drinks;
+    });
+}
+
+// EVENTS //
+
+searchButton.addEventListener('click', handleClickSearch);
