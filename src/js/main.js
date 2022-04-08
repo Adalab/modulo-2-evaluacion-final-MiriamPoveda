deca55bc319bd2d74e7203c79253b00023a4d834
MiriamPoveda <mpoveda94@gmail.com>
2022-04-08 11:20:17 +0200

    Tasks 2.2 and 2.3 added

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,37 @@
 'use strict';
 
-// PRINCIPAL VARIABLE //
+// CONSTANTS //
 
 let cocktailData = [];
 
-// CONSTANTS //
-
 const searchButton = document.querySelector('.js_search');
+const searchInput = document.querySelector('.js_searchInput');
+const listCocktails = document.querySelector('.js_listCocktails');
 
 // FUNCTIONS //
 
+/* PAINT COCKTAILS */
+function paintCocktails() {
+  let html = '';
+  for (const drink of cocktailData) {
+    html += `<li>`;
+    html += `<h3>${drink.strDrink}</h3>`;
+    html += `<img src="${drink.strDrinkThumb}" width="100"/>`;
+    html += `</li>`;
+  }
+  listCocktails.innerHTML = html;
+}
+
+/* HANDLE CLICK SEARCH + FETCH */
 function handleClickSearch(event) {
   event.preventDefault();
+  const searchValue = searchInput.value;
   /* Fetch */
-  fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
     .then((response) => response.json())
     .then((data) => {
       cocktailData = data.drinks;
+      paintCocktails();
     });
 }
 
